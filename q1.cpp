acf580e617cd770af85fa705f1b00be4e72097d2
Akash <akashsuper2000@gmail.com>
2020-11-13 09:12:46 +0530

    initial commit

---
+++ q1.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  vector<vector<string> > data = {
+    {"10", "20"},
+    {"10", "30", "50", "70"},
+    {"10", "20", "30", "40"},
+    {"10", "30", "60", "70"},
+    {"10", "50", "70"},
+    {"10", "20", "30"},
+    {"20", "40", "60"},
+    {"10", "20", "30", "60"}
+  };
+
+  map<vector<string>, int> freq[4];
+
+  for(auto row: data){
+    sort(row.begin(), row.end());
+    for(int i = 0; i<row.size(); i++){
+      freq[1][{row[i]}]++;
+      for(int j = i+1; j<row.size(); j++){
+        freq[2][{row[i], row[j]}]++;
+        for(int k = j+1; k<row.size(); k++){
+          freq[3][{row[i], row[j], row[k]}]++;
+        }
+      }
+    }
+  }
+
+  int min_samples = 3;
+  cout<<"All frequencies: \n";
+  for(int i = 1; i<=3; i++){
+    for(auto e: freq[i]){
+      for(auto word: e.first){
+        cout<<word<<' ';
+      }
+      cout<<" :  "<<e.second<<endl;
+    }
+    cout<<endl;
+  }
+
+  cout<<"Combinations with frequency greater than or equal to min_samples (3): \n";
+  for(int i = 2; i<=3; i++){
+    for(auto e: freq[i]){
+      if(e.second < min_samples) continue;
+      for(auto word: e.first){
+        cout<<word<<' ';
+      }
+      cout<<" :  "<<e.second<<endl;
+    }
+    cout<<endl;
+  }
+}
